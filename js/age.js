6f40928982b8817b39e80afb8b53ef492e18ed5e
Brett Roper <brettroper22@gmail.com>
2023-03-11 20:36:43 -0800

    work has begun

---
+++ js/age.js
@@ -0,0 +1,9 @@
+export default class user {
+    constructor(age, sex) {
+      this.age = age;
+      this.sex = sex;
+    }
+
+  }
+
+//Mercury = 4.16, Venus = 1.62, Mars = .53, Jupiter = .084, Saturn = .034, Uranus = .012, Neptune = .006
