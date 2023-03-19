877d59efc0bc7e55cc1b9882d83e431091279b22
Brett Roper <brettroper22@gmail.com>
2023-03-19 03:18:21 -0700

    Red: The test for the planet array is good failing

--- __tests__/solar.tests.js
+++ __tests__/solar.tests.js
@@ -1,12 +1,20 @@
 import user from './../js/age.js';
 
 describe('checkType', () => {
-  let Jermey;
-    beforeEach(() => {
-      Jermey = new user(42, "male");
-    });
-    test('Should be able to use checktype to return the users age and sex', () => {
-      expect(Jermey.checkType(Jermey.user)[0]).toEqual(42);
-      expect(Jermey.checkType(Jermey.user)[1]).toEqual("male");
-    });
+  let normalMale;
+  normalMale = new user(42, "male");
+
+  test('Should be able to use checktype to return the users age and sex', () => {
+    expect(normalMale.checkType()[0]).toEqual(42);
+    expect(normalMale.checkType()[1]).toEqual("male");
   });
+});
+
+describe('planets', () => {
+  let normalFemale;
+  normalFemale = new user(42, "female");
+
+  test('Should return an array with the users age across all planets', () => {
+    expect(normalFemale.planets()).toEqual([0, 0.27, 0.62, 1.88, 11.86, 29.46, 84.01, 164.8]);
+  });
+});
--- js/age.js
+++ js/age.js
@@ -8,6 +8,19 @@ export default class user {
       const returnArray = [this.age, this.sex]
       return returnArray;
     }
+
+    planets() {
+      /*const Mercury;
+      const Venus;
+      const Mars;
+      const Jupiter;
+      const Saturn;
+      const Uranus;
+      const Neptune;*/
+
+      //const planetArray = [Mercury, Venus, this.age, Mars, Jupiter, Saturn, Uranus, Neptune]
+      return planetArray;
+    }
   }
 
 //Mercury = 4.16, Venus = 1.62, Mars = .53, Jupiter = .084, Saturn = .034, Uranus = .012, Neptune = .006
