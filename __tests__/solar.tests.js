04bfce56d39cf772a8da721668df5d7d0afed251
Brett Roper <brettroper22@gmail.com>
2023-03-19 03:01:23 -0700

    Red: Good fail on checkType

--- __tests__/solar.tests.js
+++ __tests__/solar.tests.js
@@ -6,7 +6,7 @@ describe('checkType', () => {
       Jermey = new user(42, "male");
     });
     test('Should be able to use checktype to return the users age and sex', () => {
-      expect(Jermey.checktype(Jermey.user)[0]).toEqual(42);
-      expect(Jermey.checktype(Jermey.user)[1]).toEqual("male");
+      expect(Jermey.checkType(Jermey.user)[0]).toEqual(42);
+      expect(Jermey.checkType(Jermey.user)[1]).toEqual("male");
     });
   });
--- js/age.js
+++ js/age.js
@@ -1,9 +1,13 @@
 export default class user {
     constructor(age, sex) {
-      //this.age = age;
-      //this.sex = sex;
+      this.age = age;
+      this.sex = sex;
     }
 
+    checkType() {
+      //const returnArray = [this.age, this.sex]
+      return returnArray;
+    }
   }
 
 //Mercury = 4.16, Venus = 1.62, Mars = .53, Jupiter = .084, Saturn = .034, Uranus = .012, Neptune = .006
